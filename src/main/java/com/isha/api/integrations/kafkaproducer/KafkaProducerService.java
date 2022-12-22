b744311f7a89518f90398978d4ef273d51c96946
Sarath Dammala <SarathISHA@Swethas-Mac-mini.local>
2022-12-22 09:07:31 -0600

    Add some local kafka config for producer.

--- src/main/java/com/isha/api/integrations/kafkaproducer/KafkaProducerService.java
+++ src/main/java/com/isha/api/integrations/kafkaproducer/KafkaProducerService.java
@@ -1,5 +1,6 @@
 package com.isha.api.integrations.kafkaproducer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,15 @@ public class KafkaProducerService {
 
     private KafkaTemplate<String, String> template;
 
-    public KafkaProducerService(KafkaTemplate template) {
+    private String topic;
+
+    public KafkaProducerService(KafkaTemplate template, @Value("${prs.topic}") String topic) {
             this.template = template;
+            this.topic = topic;
     }
 
-    public void postMessage(String topicName, String key,  String payload) {
-        ListenableFuture<SendResult<String, String>> future = template.send(topicName, key, payload);
+    public void postMessage(String key,  String payload) {
+        ListenableFuture<SendResult<String, String>> future = template.send(this.topic, key, payload);
         future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
 
             @Override
